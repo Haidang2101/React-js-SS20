23e81832dfcc9f39ca849b53f2cad496eb97883a
Haidang2101 <haidanghanoi2003@gmail.com>
2025-09-03 00:13:47 +0700

    add

---
+++ SS20/src/App.tsx
@@ -0,0 +1,26 @@
+import Welcome from './components/Word'
+import Word from './components/UserProfile'
+import UserProfile from './components/Welcome'
+import PageTitle from './components/PageTitle'
+import Timer from './components/Timer'
+import Modal from './components/Model'
+
+
+export default function App() {
+  return (
+    <div>
+      bt1
+      <Word/>
+      bt2
+      <UserProfile/>
+      bt3
+      <Welcome/>
+      bt4
+      <PageTitle/>
+      bt5
+      <Timer/>
+      BT6
+      <Modal/>
+    </div>
+  )
+}
